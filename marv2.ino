dedc56435065619e4178f1c8ae9fedec37bf0564
shimobayashi <akimasa@shimobayashi.name>
2014-08-02 17:59:58 +0900

    Add arduino code

---
+++ marv2.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
+  
+void setup() {
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+	while (1) {}
+  }
+}
+  
+void loop() {
+    Serial.print("Temperature\t");
+    Serial.print(bmp.readTemperature());
+    
+    Serial.print("\tPressure\t");
+    Serial.print(bmp.readPressure());
+    
+    int humidity = (int)((analogRead(A0) / (1024 / 5.0)) / 1.0 * 100);
+    Serial.print("\tHumidity\t");
+    Serial.print(humidity);
+    
+    Serial.println("");
+    
+    delay(1000);
+}
