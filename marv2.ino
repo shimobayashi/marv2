951760ba03ba40c64d7578d55e2fb1ef88c504af
shimobayashi <akimasa@shimobayashi.name>
2014-08-03 23:13:39 +0900

    lower

--- marv2.ino
+++ marv2.ino
@@ -12,14 +12,14 @@ void setup() {
 }
   
 void loop() {
-    Serial.print("Temperature\t");
+    Serial.print("temperature\t");
     Serial.print(bmp.readTemperature());
     
-    Serial.print("\tPressure\t");
+    Serial.print("\tpressure\t");
     Serial.print(bmp.readPressure());
     
     int humidity = (int)((analogRead(A0) / (1024 / 5.0)) / 1.0 * 100);
-    Serial.print("\tHumidity\t");
+    Serial.print("\thumidity\t");
     Serial.print(humidity);
     
     Serial.println("");
