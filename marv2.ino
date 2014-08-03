41819961944755a4c4197c5602423ef258fd136b
shimobayashi <akimasa@shimobayashi.name>
2014-08-03 23:13:20 +0900

    even parity

--- marv2.ino
+++ marv2.ino
@@ -4,7 +4,7 @@
 Adafruit_BMP085 bmp;
   
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600, SERIAL_8E1);
   if (!bmp.begin()) {
 	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
 	while (1) {}
